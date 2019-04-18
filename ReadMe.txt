45bec262dd0428a156223de13946d4b5a23a331a
davidfstein <davidmfstein@gmail.com>
2019-04-18 15:02:39 -0400

    allows passing of initiator as arg, cleaned up write_body

--- ReadMe.txt
+++ ReadMe.txt
@@ -7,11 +7,16 @@ github into MountFolder.
 
 ### Edit the env file
 The env_file stores arguments to be passed to the container when it is run.
-The argument l, L, g, G, t, T, s, and F all correspond to arguments to the OligoMiner blockParse script, and their descriptions can be found in OligoMiner.
-seq_path corresponds to the path to the fasta file that holds your sequence.
-path_to_block_parse corresponds to the path to the blockParse.py script in OligoMiner.
-path_to_probe_generatorpy corresponds to the path to the probeGenerator.py script in ProbeGenerator.
-bed_output_path corresponds to the path at which you would like your bed file to be created by blockParse. Do not include the ".bed" extension.
+The arguments 'l', 'L', 'g', 'G', 't', 'T', 's', and 'F' all correspond to arguments to the OligoMiner blockParse script, and their descriptions can be found in OligoMiner.
+'seq_path' corresponds to the path to the fasta file that holds your sequence.
+'path_to_block_parse' corresponds to the path to the blockParse.py script in OligoMiner.
+'path_to_probe_generatorpy' corresponds to the path to the probeGenerator.py script in ProbeGenerator.
+'bed_output_path' corresponds to the path at which you would like your bed file to be created by blockParse. Do not include the ".bed" extension.
+'initiator' correspond to the name of the amplifier you would like to use.
+'left_init_seq' is the left amplifier sequence.
+'left_spacer' is the spacer to be used with the left amplifier sequence
+'right_init_seq' is the right amplifier sequence
+'right_spacer' is the spacer to be used with the right amplifier sequence
 Update the env file with the appropriate arguments for your needs. 
 
 ### Run the container
@@ -19,3 +24,25 @@ Update the env file with the appropriate arguments for your needs.
 docker run --env-file={path/to/env_file} -v ~/{path/to/MountFolder}:/app dstein96/probegenerator:latest
 ```
 After running the container you should see a csv file containing the probe information in your MountFolder.
+
+### Troubleshooting
+If a new version of the probegenerator is released on docker you will need to pull the latest image in order to
+utilize the latest functionality. There are two ways to achieve this. The latest version of the image will be accessible under two tags,
+'latest' and the highest numerical version value. For instance, if there are images tagged with 0.1 and 0.2 and latest, then both 
+the image tagged 'latest' and '0.2' correspond to the most up to date image. If you have never used the image with the tag you would like to
+access, then you can simply update the run command to:
+```
+docker run --env-file={path/to/env_file} -v ~/{path/to/MountFolder}:/app dstein96/probegenerator:{tag}
+```
+If you have used the tagged version before, but it has been updated then you will need to delete the old image from 
+your local docker repository. To do so run the following command:
+```
+docker rmi dstein96/probegenerator:{tag}
+```
+Now when you run the 'docker run' command, docker will pull in the latest version from the remote repository.
+
+Similarly, if the probegenerator code is updated on github, you will need to pull the latest version. You can either delete the probegenerator 
+folder and clone it again, or simply from the root of probegenerator run:
+```
+git pull origin master
+```
--- probegenerator/probeGenerator.py
+++ probegenerator/probeGenerator.py
@@ -4,7 +4,8 @@ import sys
 import csv
 
 ## TODO : get amplifier as arg
-def getProbePairs(input_path, desired_spaces):
+def getProbePairs(input_path, desired_spaces, initiator_name, left_initiator_seq, left_initiator_spacer, 
+                    right_initiator_seq, right_initiator_spacer):
     seqf=[]
     probeend = 0
     with open (input_path) as gene:
@@ -14,7 +15,7 @@ def getProbePairs(input_path, desired_spaces):
                 seqf.append(seqn)
                 probeend = (int(seqn[2]) + int(desired_spaces))
     pairs = create_pairs(seqf)
-    write_probes_to_csv(pairs, desired_spaces)
+    write_probes_to_csv(pairs, desired_spaces, initiator_name, left_initiator_seq, left_initiator_spacer, right_initiator_seq, right_initiator_spacer)
 
 def create_pairs(sequences): 
     if not sequences:
@@ -32,54 +33,56 @@ def create_pairs(sequences):
             pairs.append([previous_sequence, sequence])
         previous_sequence = sequence    
 
-    # TODO : just pass these as pairs to make write body easier
-    return [seq for pair in pairs for seq in pair]
+    return pairs
 
-
-def write_probes_to_csv(seq_pairs, desired_spaces):
+def write_probes_to_csv(seq_pairs, desired_spaces, initiator_name, left_initiator_seq, left_initiator_spacer, right_initiator_seq, right_initiator_spacer):
     with open('./probes.csv', 'w+') as probes:
         writer = csv.writer(probes, delimiter=",")
-        writer.writerow(['SHH', 'start', 'stop', 'seq', 'tm', 'spacing', 'underscore', 'set', 'dot', 'probe', 'amplifier', 'final name', 'left', 'spacer', 'right', 'final probe'])
-        write_body(writer, seq_pairs, desired_spaces)
+        writer.writerow(['gene name', 'start', 'stop', 'seq', 'tm', 'spacing', 'set', 'probe', 'amplifier', 'final name', 'left', 'spacer', 'right', 'final probe'])
+        write_body(writer, seq_pairs, initiator_name, left_initiator_seq, left_initiator_spacer, right_initiator_seq, right_initiator_spacer)
 
-def write_body(writer, seqf, desired_spaces):
-    last_index = 0
-    set_num = 0
-    for i in range(0, len(seqf)):
-        line = seqf[i]
-        line[3] = reverseComplement(line[3])
-        dot = "."
-        underscore = "_"
-        amplifier = "_B4"
-        space = int(line[1]) - int(last_index)
-        left = ''
-        right = ''
-        spacer = ''
-        if space >= desired_spaces:
-            continue
-        if set_num == 0:
-            set_num = 1
-        if i >= 2 and i % 2 == 0:
-            set_num += 1
-        ## do this better
-        if i == 0:
-            probe_num = 1
-        else:
-            probe_num = (i % 2) + 1
-        if probe_num == 1:
-            left = 'CCTCAACCTACCTCCAAC'
-            spacer = 'AA'
-            right = line[3]
-        else:
-            left = line[3]
-            spacer = 'AT'
-            right = 'TCTCACCATATTCgCTTC'
-        
-        last_index = line[2]
-        final_name = line[0] + underscore + str(set_num) + dot + str(probe_num) + amplifier
-        final_probe = left + spacer + right
-        writer.writerow([line[0], line[1], line[2], line[3], line[4], space, underscore, set_num, dot, probe_num, amplifier, final_name, left, spacer, right, final_probe])
+def write_body(writer, pairs, initiator_name, left_initiator_seq, left_initiator_spacer, right_initiator_seq, right_initiator_spacer):
+    last_seq_end = 0
+    for index in range(0, len(pairs)):
+        left = pairs[index][0]
+        right = pairs[index][1]
+        left_space = int(left[1]) - int(last_seq_end)
+        right_space = int(right[1]) - int(left[2])
+        set_num = index + 1
+        left_final_name = left[0] + "_" + str(set_num) + "." + str(1) + "_" + initiator_name
+        right_final_name = right[0] + "_" + str(set_num) + "." + str(2) + "_" + initiator_name 
+        left_final_probe = left[3] + right_initiator_spacer + right_initiator_seq
+        right_final_probe = left_initiator_seq + left_initiator_spacer + right[3]
+        writer.writerow([left[0], 
+                        left[1], 
+                        left[2], 
+                        left[3], 
+                        left[4], 
+                        left_space, 
+                        set_num, 
+                        1, 
+                        "_" + initiator_name, 
+                        left_final_name, 
+                        left[3], 
+                        right_initiator_spacer, 
+                        right_initiator_seq, 
+                        left_final_probe])
+        writer.writerow([right[0], 
+                        right[1], 
+                        right[2], 
+                        right[3], 
+                        right[4], 
+                        right_space, 
+                        set_num, 
+                        2, 
+                        "_" + initiator_name, 
+                        right_final_name, 
+                        left_initiator_seq, 
+                        left_initiator_spacer, 
+                        right[3], 
+                        right_final_probe])
+        last_seq_end = right[2]
 
 if __name__ == '__main__':
-    inputPath, desiredSpaces = parseArgs(sys.argv[1:])
-    getProbePairs(inputPath, desiredSpaces)
+    inputPath, desiredSpaces, initiator_name, left_initiator_seq, left_initiator_spacer, right_initiator_seq, right_initiator_spacer = parseArgs(sys.argv[1:])
+    getProbePairs(inputPath, desiredSpaces, initiator_name, left_initiator_seq, left_initiator_spacer, right_initiator_seq, right_initiator_spacer)
