474f07fbaa4a867645ec3c5391e3cf81df185ab4
davidfstein <davidmfstein@gmail.com>
2019-05-07 21:25:07 -0400

    parses a multifasta file and writes individual fasta files to disk

--- probegenerator/parseMultifasta.py
+++ probegenerator/parseMultifasta.py
@@ -1,4 +1,5 @@
-import argparse
+from __future__ import print_function
+from argparse import ArgumentParser
 from collections import namedtuple
 
 def write_to_separate_fasta_files(fasta_entries):
@@ -33,13 +34,13 @@ def concat_lines(lines):
     return concatted_line
 
 def gene_name_from_header(line):
-    return line[1:]
+    return line[1:].split(' ')[0]
 
 def is_header(line):
     return '>' in line
 
 def main():
-    userInput = argparse.ArgumentParser(description="Requires a fasta file or multifasta file as input. If file is multifasta, the file is split "
+    userInput = ArgumentParser(description="Requires a fasta file or multifasta file as input. If file is multifasta, the file is split "
                                                     + "into mulitple single entry fasta files and the new fasta files are written to the current directory. "
                                                     + "Returns a list of resulting file names for passing to downstream tools.")
     requiredNamed = userInput.add_argument_group('required arguments')
@@ -50,7 +51,7 @@ def main():
     
     with open(inputFile) as file:
         fasta_entries = multifasta_to_sequence_list(file.readlines())
-        return write_to_separate_fasta_files(fasta_entries)
+        print(*write_to_separate_fasta_files(fasta_entries))
 
 if __name__ == '__main__':
     main()
