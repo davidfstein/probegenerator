494f937a3c25ca1c23d5639809c87b88970bc287
davidfstein <davidmfstein@gmail.com>
2019-05-27 18:20:21 -0400

    markdown for readme, add args and remove unneccesary args for script

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-fasta_files=$(python $1 -f $2)
+fasta_files=$(python $1/probegenerator/parseMultifasta.py -f $2)
 declare -a arr=($fasta_files)
-export BOWTIE2_INDEXES=/app/M21-transcriptome/
+export BOWTIE2_INDEXES=${18}
 for fasta in "${arr[@]}"
 do 
     # Consider turning off overlap mode of blockParse
-    python $3 -f "$fasta.fa" -l $4 -L $5 -g $6 -G $7 -t $8 -T $9 -s ${10} -F ${11} -O -b -o ${12}
-    python ${13} -p ${14} -s ${15} -i ${16} -l ${17} --LeftSpacer ${18} -r ${19} --RightSpacer ${20}
-    bowtie2 -x M21-transcriptome -U ../probes_for_alignment.fa -f --very-sensitive > ../output.bam
-    python ./probegenerator/probegenerator/parseBam.py -p ../output.bam
-    python ./probegenerator/probegenerator/probeWriter.py --TagFile ../probe_scores.txt --Probes ../probes_with_meta.txt
+    python $3 -f $fasta.fa -l $4 -L $5 -g $6 -G $7 -t $8 -T $9 -s ${10} -F ${11} -O -b -o ../output
+    python $1/probegenerator/probeGenerator.py -p ../output.bed -s ${12} -i ${13} -l ${14} --LeftSpacer ${15} -r ${16} --RightSpacer ${17}
+    bowtie2 -x ${19} -U ../probes_for_alignment.fa -f --very-sensitive > ../output.bam
+    python $1/probegenerator/parseBam.py -p ../output.bam
+    python $1/probegenerator/probeWriter.py --TagFile ../probe_scores.txt --Probes ../probes_with_meta.txt
 done
 
