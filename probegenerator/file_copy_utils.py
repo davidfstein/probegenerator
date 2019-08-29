91731b4ae365926384fbe7289ba8b21f45cfa2b9
David Stein <davidmfstein@gmail.com>
2019-08-29 16:46:42 -0400

    copy bam to appropriate initiator and gene directories

---
+++ probegenerator/file_copy_utils.py
@@ -0,0 +1,28 @@
+from shutil import copyfile
+from argparse import ArgumentParser
+import csv
+import os
+
+def main():
+    userInput = ArgumentParser(description="")
+    requiredNamed = userInput.add_argument_group('required arguments')
+    requiredNamed.add_argument('-i', '--Initiators', action='store', required=True)
+    requiredNamed.add_argument('-n', '--Name', action='store', required=True)
+
+    args = userInput.parse_args()
+    initiator_file = args.Initiators
+    copy_file_name = args.Name
+
+    initiators = []
+    with open(initiator_file) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            initiators.append([ row['initiator'], row['left sequence'], row['left spacer'], row['right sequence'], row['right spacer'] ]) 
+    
+    for initiator in initiators:
+        copyfile(copy_file_name, os.path.join('output', initiator[0], copy_file_name))
+
+    os.remove(copy_file_name)
+
+if __name__ == '__main__':
+    main()
