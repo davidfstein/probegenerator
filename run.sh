78452a1b18a57aec97e33eb5013cc11b94ecd2c8
David Stein <davidmfstein@gmail.com>
2019-08-29 23:28:12 -0400

    hardcode path to projects

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+
 mkdir /data/output
-fasta_files=$(python $1/probegenerator/parseMultifasta.py -f $2)
+fasta_files=$(python /app/probegenerator/probegenerator/parseMultifasta.py -f $1)
 declare -a arr=($fasta_files)
-export BOWTIE2_INDEXES=${14}
+export BOWTIE2_INDEXES=${12}
 for fasta in "${arr[@]}"
 do 
-    python $3 -f $fasta.fa -l $4 -L $5 -g $6 -G $7 -t $8 -T $9 -s ${10} -F ${11} -O -b -o ../output
+    python /app/OligoMiner/blockParse.py -f $fasta.fa -l $2 -L $3 -g $4 -G $5 -t $6 -T $7 -s $8 -F $9 -O -b -o ../output
     splitFilePathArr=(${fasta//\// })
-    python $1/probegenerator/probeGenerator.py -p ../output.bed -f $fasta.fa -s ${12} -if ${13}
-    bowtie2 -x ${15} -U ../probes_for_alignment.fa -t -f --very-sensitive -k 5 --int-quals --no-1mm-upfront --score-min L,-40,-0.6 -p 4 > "${splitFilePathArr[1]}".bam
-    python $1/probegenerator/file_copy_utils.py -i ${13} -n "${splitFilePathArr[1]}"
+    python /app/probegenerator/probegenerator/probeGenerator.py -p ../output.bed -f $fasta.fa -s ${10} -if ${11}
+    bowtie2 -x ${13} -U ../probes_for_alignment.fa -t -f --very-sensitive -k 5 --int-quals --no-1mm-upfront --score-min L,-40,-0.6 -p 4 > "${splitFilePathArr[1]}".bam
+    python /app/probegenerator/probegenerator/file_copy_utils.py -i ${11} -n "${splitFilePathArr[1]}"
 done
