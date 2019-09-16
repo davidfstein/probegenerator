32c7baf34327f46b30741b66cb17d44f4db43509
David Stein <davidmfstein@gmail.com>
2019-09-16 14:55:13 -0400

    added contents

--- probegenerator/utils/file_writer_utils.py
+++ probegenerator/utils/file_writer_utils.py
@@ -6,6 +6,10 @@ import csv
 import os
 
 def write_probes_for_alignment_fasta(pairs, desired_spaces):
+    '''
+    Writes probes to disk in the fasta format. Probes are seperated into pairs and
+    fasta headers are use the pair set numbers as identifiers.
+    '''
     with open('../probes_for_alignment.fa', 'w+') as file:
         for index in range(len(pairs)):
             file.write('>pair' + str(index + 1) + '\n')
@@ -13,6 +17,9 @@ def write_probes_for_alignment_fasta(pairs, desired_spaces):
             file.write(reverseComplement(pairs[index][1][3]) + spacer + reverseComplement(pairs[index][0][3]) + '\n')
 
 def write_probes_to_csv(pairs, path='.'):
+    '''
+    Writes probe pairs with metadata to a csv file.
+    '''
     name = pairs[0][0][0].split(" ")[0] + '_probes.csv'
     with open(os.path.join(path, name), 'w+') as probes:
         writer = csv.writer(probes, delimiter=",")
@@ -25,6 +32,9 @@ def write_body(writer, pairs):
         writer.writerow(pair[1])
 
 def write_specific_probes(path, probes, initiator):
+    '''
+    Writes probes to disk in the fasta format. Probes grouped by gene and initiator.
+    '''
     name = probes[0]['gene name'].split(" ")[0]
     with open(os.path.join(path, name, name) + '.fasta', 'w+') as f:
         f.write(">" + name + " probes initiator " + initiator + "\n")
@@ -33,6 +43,10 @@ def write_specific_probes(path, probes, initiator):
             f.write(probes[i+1]['final probe'] + '\n')
 
 def write_fasta(records):
+    '''
+    Write fasta records as individual fasta files on disk. Return the names of the files 
+    without the file extenstion.
+    '''
     names = []
     for record in records:
         name = '../' + strip_filename_illegal_characters(record.name) + '.fa'
@@ -42,6 +56,10 @@ def write_fasta(records):
     return names
 
 def strip_filename_illegal_characters(name):
+    '''
+    Strip some illegal characters from file names. May not be consistent across operating
+    systems. 
+    '''
     name = name.replace('/', '').replace(':', '').replace('\\', '').replace('.', '').replace('|', '')
     return name
 
