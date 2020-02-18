8d1a0cd35c4064d5b97a4b77ca6ab03775dcabc9
davidfstein <davidmfstein@gmail.com>
2020-02-18 11:48:05 -0500

    optional functionality for web app

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+export AWS_ACCESS_KEY_ID=${15}
+export AWS_SECRET_ACCESS_KEY=${16}
+export AWS_DEFAULT_REGION=${17}
+echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+source ./sendgrid.env
+
+if [ ! -z "${14}" ]
+then
+    aws s3 cp s3://probegenerator-jobs/$1 /data/$1
+    # unzip $1.zip
+fi    
+
 if [ -d "/data/output" ] 
 then
     echo "Please remove or move the output folder from your mount directory." && exit 1
@@ -19,3 +31,10 @@ do
     python /app/probegenerator/probegenerator/utils/file_copy_utils.py -i /data/${11} -n "${gene_name[1]}"
     python /app/probegenerator/probegenerator/parseBam.py -p ${gene_name[1]}/${gene_name[1]}_probes.csv -p2 ${gene_name[1]}/${gene_name[1]}.bam -i /data/${11}
 done
+
+zip -r /data/results.zip /data/output
+
+if [ ! -z "${18}" ]
+then
+    python /app/probegenerator/probegenerator/utils/mail_utils.py -r david.f.stein@gmail.com
+fi
