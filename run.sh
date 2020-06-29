4257e6402ef91bbf4bcd13c368e72259bc071478
David Stein <davidmfstein@gmail.com>
2020-06-29 13:04:39 -0400

    read fasta file names from file

--- run.sh
+++ run.sh
@@ -12,17 +12,16 @@ mkdir /data/output
 
 export BOWTIE2_INDEXES=/data/${12}
 
-fasta_files=$(python /app/probegenerator/probegenerator/parseMultifasta.py -f /data/$1)
-declare -a gene_fastas=($fasta_files)
-for fasta in "${gene_fastas[@]}"
+python /app/probegenerator/probegenerator/parseMultifasta.py -f /data/$1
+while read -r fasta
 do 
     python /app/OligoMiner/blockParse.py -f $fasta.fa -l $2 -L $3 -g $4 -G $5 -t $6 -T $7 -s $8 -F $9 -O -b -o ../output
     gene_name=(${fasta//\// })
     python /app/probegenerator/probegenerator/probeGenerator.py -p ../output.bed -f $fasta.fa -s ${10} -if /data/${11}
-    bowtie2 -x ${13} -U ../probes_for_alignment.fa -t -f --very-sensitive -k 5 --int-quals --no-1mm-upfront --score-min L,-40,-0.6 -p 4 > "${gene_name[1]}".bam
+    bowtie2 -x ${13} -U ../probes_for_alignment.fa -t -f --very-sensitive -k 5 --int-quals --no-1mm-upfront --score-min L,-40,-0.6 -p 4  > "${gene_name[1]}".bam
     python /app/probegenerator/probegenerator/utils/file_copy_utils.py -i /data/${11} -n "${gene_name[1]}"
     python /app/probegenerator/probegenerator/parseBam.py -p ${gene_name[1]}/${gene_name[1]}_probes.csv -p2 ${gene_name[1]}/${gene_name[1]}.bam -i /data/${11}
-done
+done < /app/names.txt
 
 zip -r /data/results.zip /data/output
 
