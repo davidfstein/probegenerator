d00dad583ebf598f61af03ea905355798b1f680a
David Stein <davidmfstein@gmail.com>
2020-06-30 15:54:10 -0400

    support bed processing

--- probegenerator/utils/file_copy_utils.py
+++ probegenerator/utils/file_copy_utils.py
@@ -12,10 +12,12 @@ def main():
     requiredNamed = userInput.add_argument_group('required arguments')
     requiredNamed.add_argument('-i', '--Initiators', action='store', required=True)
     requiredNamed.add_argument('-n', '--Name', action='store', required=True)
+    requiredNamed.add_argument('-e', '--Extension', action='store', required=True)
 
     args = userInput.parse_args()
     initiator_file = args.Initiators
     copy_file_name = args.Name
+    extension = args.Extension
 
     initiators = []
     with open(initiator_file) as file:
@@ -24,7 +26,7 @@ def main():
             initiators.append([ row['initiator'], row['left sequence'], row['left spacer'], row['right sequence'], row['right spacer'] ]) 
     
     for initiator in initiators:
-        copyfile(copy_file_name + '.bam', os.path.join('/data', 'output', initiator[0], copy_file_name, copy_file_name + '.bam'))
+        copyfile(copy_file_name + extension, os.path.join('/data', 'output', initiator[0], copy_file_name, copy_file_name + extension))
 
 if __name__ == '__main__':
     main()
